05a3afdbc71dd4ce98fac9d60e19f6c002732bd4
Ronasian <119774818+Ronasian@users.noreply.github.com>
2023-01-20 15:09:30 -0800

    updated HTML content and added CSS styling

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var greetingEl = document.querySelector(".quiz-greeting");
+var quizEl = document.querySelector(".quiz-content");
+var finishedEl = document.querySelector(".quiz-complete");
+var startEl = document.querySelector("#start-button");
+
